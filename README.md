51cf459e722d9d0ad20da1d0d59e4ef56e1f9a4e
arteck <6681528+arteck@users.noreply.github.com>
2026-07-16 17:54:49 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
+<img src="admin/anycubic.png" width="200" />
+
 # ioBroker.anycubic
-ws connection to anycubic (need rinkhals)
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.anycubic.svg)](https://www.npmjs.com/package/iobroker.anycubic)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.anycubic.svg)](https://www.npmjs.com/package/iobroker.anycubic)
+![Number of Installations](https://iobroker.live/badges/anycubic-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/anycubic-stable.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.anycubic.png?downloads=true)](https://nodei.co/npm/iobroker.anycubic/)
+
+**Tests:**  
+![Test and Release](https://github.com/arteck/ioBroker.anycubic/workflows/Test%20and%20Release/badge.svg)
+![CodeQL](https://github.com/arteck/ioBroker.anycubic/actions/workflows/codeql.yml/badge.svg?branch=main)
+
+## Anycubic adapter for ioBroker
+
+
+
+## License
+
+MIT License
+
+Copyright (c) 2026 Arthur Rupp <arteck@outlook.com>,
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,189 @@
+'use strict';
+
+const core = require('@iobroker/adapter-core');
+const {StatesController} = require('./lib/statesController');
+const {WebsocketController} = require('./lib/websocketController');
+const {Helper} = require('./lib/helper');
+
+
+class anycubic extends core.Adapter {
+    constructor(options) {
+        super({
+            ...options,
+            name: 'anycubic',
+        });
+
+        // Instanz-State statt Modul-globale Variablen
+        this.deviceCache = {};
+        this.websocketController = null;
+        this.statesController = null;
+        this.helper = null;
+        this.messageParseMutex = Promise.resolve();
+        this.parseOptions = {write: false};
+        this.startListening = false;
+        this.allNodesCreated = false;
+        this.nodeCache = {};
+
+        this.on('ready', () => {
+            this.onReady().catch((e) => this.log.error(`onReady error: ${e}`));
+        });
+        this.on('stateChange', (id, state) => {
+            this.onStateChange(id, state).catch((e) => this.log.error(`onStateChange error: ${e}`));
+        });
+
+        this.on('unload', this.onUnload.bind(this));
+
+    }
+
+    async onReady() {
+        this.statesController = new StatesController(this);
+
+        this.setStateChanged('info.connection', false, true);
+        await this.statesController.setAllAvailableToFalse();
+
+        this.helper = new Helper(this, this.deviceCache);
+
+        if (this.config.wsOnStart) {
+            this.setStateChanged('info.sendMessageAllowed', true, true);
+        }
+
+        this.setStateChanged('info.debugmessages', '', true);
+
+        // WebSocket-Verbindung
+        if (!this.config.wsServerIP) {
+            this.log.warn('Please configure the Websocket connection!');
+            return;
+        }
+
+        this.startWebsocket();
+    }
+
+
+    startWebsocket() {
+        this.websocketController = new WebsocketController(this);
+        const wsClient = this.websocketController.initWsClient();
+
+        if (!wsClient) {
+            this.log.error('<anycubic> initWsClient returned null — websocket not started.');
+            return;
+        }
+
+        wsClient.on('open', () => {
+            this.startListening = true;
+        });
+
+        wsClient.on('message', (message) => {
+            this.messageParse(message);
+        });
+
+        wsClient.on('close', async () => {
+            this.setStateChanged('info.connection', false, true);
+            this.startListening = false;
+            this.allNodesCreated = false;
+            this.deviceCache = {};
+            this.nodeCache = {};
+            this.log.info('Websocket connection closed. Attempting to reconnect...');
+        });
+    }
+
+    async messageParse(message) {
+        const lock = new Promise((resolve) => resolve());
+        const prev = this.messageParseMutex;
+        this.messageParseMutex = lock;
+        await prev;
+
+        try {
+            let messageObj = JSON.parse(message);
+
+            this.log.debug(`--->>> fromZ2W_RAW_1 -> ${JSON.stringify(messageObj)}`);
+
+            const type = messageObj?.type;
+
+            // TODO: Process message based on type
+            if (!type) {
+                this.log.warn(`<anycubic> received message without type: ${JSON.stringify(messageObj)}`);
+            }
+        } catch (err) {
+            this.log.error(err);
+            this.log.error(`<anycubic> error message -->> ${message}`);
+        }
+    }
+
+    async onUnload(callback) {
+        try {
+            if (this.websocketController) {
+                try {
+                    await this.websocketController.allTimerClear();
+                    this.websocketController.closeConnection();
+                } catch (e) {
+                    this.log.error(e);
+                }
+            }
+
+            try {
+                if (this.statesController) {
+                    await this.statesController.setAllAvailableToFalse();
+                }
+            } catch (e) {
+                this.log.error(e);
+            }
+
+            this.setStateChanged('info.connection', false, true);
+        } finally {
+            callback();
+        }
+    }
+
+
+    async onStateChange(id, state) {
+        if (!this.allNodesCreated) {
+            return;
+        }
+
+        if (state && state.ack === false) {
+            if (id.endsWith('info.debugId')) {
+                this.setStateChanged(id, state.val, true);
+                return;
+            }
+
+            const obj = await this.getObjectAsync(id);
+            if (obj) {
+                const nativeObj = obj.native || {};
+
+                const m = id.match(/nodeID_0*(\d+)/i);
+                if (!m) {
+                    this.log.warn(`<anycubic> Could not extract nodeId from state id: ${id}`);
+                    return;
+                }
+                const nodeId = Number(m[1]);
+
+                const message = {
+                    messageId: `${Date.now()}-${Math.random().toString(16).slice(2)}`,
+                    command: 'node.set_value',
+                    nodeId,
+                    valueId: nativeObj.valueId,
+                    value: state.val,
+                };
+
+                const sendMessageAllowed = await this.getStateAsync('info.sendMessageAllowed');
+
+                if (sendMessageAllowed && sendMessageAllowed.val === true) {
+                    if (this.websocketController) {
+                        this.websocketController.send(JSON.stringify(message));
+                    } else {
+                        this.log.warn('<anycubic> websocketController not initialised, cannot send message.');
+                    }
+                }
+
+                this.setStateChanged('info.debugmessages', JSON.stringify(message), true);
+                this.log.debug(`<anycubic> message onStateChange ${JSON.stringify(message)}`);
+            }
+        }
+    }
+}
+
+if (require.main !== module) {
+    module.exports = (options) => new anycubic(options);
+} else {
+    new anycubic();
+}
